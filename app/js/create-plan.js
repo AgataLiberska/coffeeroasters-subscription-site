832814eccfc8ea4d331ad9e09a718a1b2d449df2
Agata Liberska <liberska@gmail.com>
2021-03-03 23:53:14 +0000

    update order summary as form is filled

--- app/js/create-plan.js
+++ app/js/create-plan.js
@@ -1,16 +1,129 @@
+// accordion butttons
 const accordionToggles = document.querySelectorAll('.js-accordion-toggle');
 
+// all radio inputs
+const formInputs = document.querySelectorAll('input');
 
-// open & close accordions - add class and aria-expanded
-accordionToggles.forEach(toggle => {
-    toggle.addEventListener('click', () => {
-        if (toggle.classList.contains('is-open')) {
-            toggle.classList.remove('is-open');
-            toggle.setAttribute("aria-expanded", "false")
+// order summary fields
+const preference = document.querySelector('.js-preference');
+const preferenceIntro = document.querySelector('.js-preference-intro');
+const beanType = document.querySelector('.js-bean-type');
+const quantity = document.querySelector('.js-quantity');
+const grind = document.querySelector('.js-grind');
+const grindIntro = document.querySelector('.js-grind-intro');
+const delivery = document.querySelector('.js-delivery');
+
+// elements which need to be disabled if 'capsule' is chosen
+const grindAccordion = document.querySelector('.js-grind-toggle');
+const grindInputs = document.querySelectorAll('.js-grind-input');
+
+
+// FUNCTIONS =========================================
+
+function expand(accordion) {
+    accordion.classList.add('is-open');
+    accordion.setAttribute("aria-expanded", "true");
+}
+
+function collapse(accordion) {
+    accordion.classList.remove('is-open');
+    accordion.setAttribute("aria-expanded", "false");
+}
+
+function disableAccordion(accordion) {
+    accordion.setAttribute("disabled", "");
+    accordion.setAttribute("tabindex", "-1");
+}
+
+function enableAccordion(accordion) {
+    accordion.removeAttribute("disabled");
+    accordion.setAttribute("tabindex", "0");
+}
+
+function disableInput(input) {
+    input.setAttribute("disabled", "");
+    input.removeAttribute("required");
+}
+
+function enableInput(input) {
+    input.removeAttribute("disabled");
+    input.setAttribute("required", "");
+}
+
+function updateSummary(field, content) {
+    field.textContent = content;
+}
+
+
+// EVENT LISTENERS ===================================
+
+accordionToggles.forEach(accordion => {
+    accordion.addEventListener('click', () => {
+        if (accordion.classList.contains('is-open')) {
+            collapse(accordion);
         } else {
-            toggle.classList.add('is-open');
-            toggle.setAttribute("aria-expanded", "true");
+            expand(accordion);
         }
-        
+    })
+})
+
+formInputs.forEach(input => {
+    input.addEventListener('change', e => {
+        switch (e.target.name) {
+            case 'preferences': 
+                if (e.target.value === 'capsules') {
+                    disableAccordion(grindAccordion);
+                    collapse(grindAccordion);
+                    grindInputs.forEach(input => disableInput(input));
+
+                    updateSummary(preferenceIntro,"using ");
+                    
+                    // remove grind from summary if capsules are selected
+                    updateSummary(grindIntro, "");
+                    updateSummary(grind, "");
+                } 
+                
+                else {
+                    enableAccordion(grindAccordion);
+                    grindInputs.forEach(input => enableInput(input));
+
+                    preferenceIntro.textContent = "as ";
+
+                    // don't add ground ala if wholebean has been chosen
+                    if (grind.textContent !== 'wholebean') {
+                        updateSummary(grindIntro, " ground ala");
+                    }
+                    
+                    // only add empty field when no option has been chosen
+                    if (!grind.textContent) {
+                        updateSummary(grind, "_____");
+                    }
+                }
+                updateSummary(preference, e.target.value);
+                break;
+
+            case 'bean-type':
+                updateSummary(beanType, e.target.value);
+                break;
+
+            case 'quantity':
+                updateSummary(quantity, e.target.value);
+                break;
+
+            case 'grind':
+                // remove 'ground ala' from order summary for wholebean
+                if (e.target.value === 'wholebean') {
+                    updateSummary(grindIntro, "");
+                }                 
+                else {
+                    updateSummary(grindIntro, " ground ala");
+                }
+                updateSummary(grind, e.target.value);
+                break;
+
+            case 'delivery': 
+                updateSummary(delivery, e.target.value);
+                break;
+        }
     })
 })
