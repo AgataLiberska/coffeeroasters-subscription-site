c09b6db4a10aa520a2f6ee96873bb3ff6e931bed
Agata Liberska <liberska@gmail.com>
2021-03-03 11:22:13 +0000

    add form styles for desktop

--- app/js/create-plan.js
+++ app/js/create-plan.js
@@ -1,6 +1,7 @@
 const accordionToggles = document.querySelectorAll('.js-accordion-toggle');
 
 
+// open & close accordions
 accordionToggles.forEach(toggle => {
     toggle.addEventListener('click', () => {
         toggle.classList.toggle('is-open');
